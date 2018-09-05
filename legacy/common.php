239c5102791b9b9ab22e38fc827adc584362a0af
Oguzhan Uysal <development@oguzhanuysal.eu>
2018-09-05 13:11:24 +0200

    use composer as psr-0 autoloader

--- legacy/common.php
+++ legacy/common.php
@@ -2,18 +2,6 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 
-set_include_path(realpath('lib').PATH_SEPARATOR.get_include_path());
-set_include_path(realpath('classes').PATH_SEPARATOR.get_include_path());
-
-spl_autoload_register(function($className) {
-	$className = str_replace("\\", DS, $className);
-	$filename = str_replace('_', DS, $className).'.php';
-
-	if(stream_resolve_include_path($filename)) {
-		include $filename;
-	}
-});
-
 require_once(__DIR__.'/../vendor/autoload.php');
 
 if(php_sapi_name() !== 'cli') {
@@ -26,6 +14,5 @@ $config = \Config::get();
 
 \R::setup($config->db->dsn, $config->db->user, $config->db->pass);
 \R::freeze(true);
-
 
 ?>
